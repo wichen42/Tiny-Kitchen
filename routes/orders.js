fa93d4465a810866ffd8ecd511fcbf37e35c14a6
Wilson Chen <wilsonchen901@gmail.com>
2022-12-06 14:54:32 -0500

    create patch route

--- routes/orders.js
+++ routes/orders.js
@@ -20,8 +20,11 @@ router.post('/', (req, res) => {
     .catch(err => res.status(422).json(err));
 
 });
-router.patch('/', (req, res) => {
-    res.json('PATCH Success');
+router.patch('/:order_id', (req, res) => {
+  const id = req.params.order_id;
+  Order.findOneAndUpdate(id)
+    .then(order => res.json({id: order._id}))
+    .catch(err => req.status(404).json(err));
 });
 router.delete('/:order_id', (req, res) => {
     const id = req.params.order_id;
